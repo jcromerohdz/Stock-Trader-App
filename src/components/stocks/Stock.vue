904d801215488c51ca660e41e2a2f9d31d290098
J. Christian Romero <jcromerohdz@gmail.com>
2020-05-26 10:45:23 -0700

    Adding content to the Stock component

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,14 +1,65 @@
 <template>
-<h1>Stock/Stocks component</h1>
+  <div class="col col-sm-6 col-md-4">
+    <div class="card mb-3">
+      <div class="card-header alert-success">{{stock.name}} (Price: {{stock.price}})</div>
+      <div class="card-body">
+        <div class="input-group mb-3">
+          <input
+            type="number"
+            class="form-control"
+            placeholder="Quantity"
+            aria-label="Quantity"
+            aria-describedby="basic-addon2"
+            v-model="quantity"
+          />
 
+          <div class="pull-right">
+            <!-- <div class="input-group-text" id="btnGroupAddon" disabled>@</div>
+            <span class="input-group-text alert-success" 
+            id="basic-addon2"
+            @click="buyStock"
+            :disabled="quantity <= 0 || !Number.isInteger(quantity)">
+            Buy
+            </span> -->
+            <button type="button" 
+              class="btn btn-success"
+              @click="buyStock"
+              :disabled="quantity <= 0"
+              >
+              Buy
+              </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  
-}
+  props: ['stock'],
+  data() {
+    return {
+      text: "",
+      quantity: 0
+    };
+  },
+  methods: {
+    buyStock(){
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      }
+      console.log(order)
+      this.quantity = 0
+    }
+  }
+};
 </script>
 
 <style scoped>
-
+#box {
+  padding: 15px;
+}
 </style>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,11 +1,31 @@
 <template>
-<h1>Stocks component</h1>
+  <div class="container">
+    <div class="row">
+
+    <appStock v-for="s in stocks" :key="s.id" :stock="s"></appStock>
+    </div>
+  </div>
 
 </template>
 
 <script>
+import Stock from './Stock'
+
 export default {
-  
+
+  data() {
+    return {
+      stocks: [
+        {id:1, name: "BMW", price: 110},
+        {id:2, name: "Google", price: 250},
+        {id:3, name: "Apple", price: 300},
+        {id:4, name: "Twitter", price: 200}
+      ]
+    }
+  },
+  components: {
+    appStock: Stock
+  }
 }
 </script>
 
